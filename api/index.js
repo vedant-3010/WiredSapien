347ca7009f613b37cef91f4a1b2667665aebb8ed
Vedant <vedantdandge11@gmail.com>
2023-03-19 02:02:13 +0530

    login&signup added

--- api/index.js
+++ api/index.js
@@ -4,15 +4,20 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const User = require("./models/User");
 const Post = require("./models/Post");
+const bcrypt = require("bcryptjs");
 
+const salt = bcrypt.genSaltSync(10);
 const port = 4000;
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect( "mongodb://vedant:iaK7qavvID8cbhyl@ac-dgelwon-shard-00-00.owb0mze.mongodb.net:27017,ac-dgelwon-shard-00-01.owb0mze.mongodb.net:27017,ac-dgelwon-shard-00-02.owb0mze.mongodb.net:27017/?ssl=true&replicaSet=atlas-4gp2jm-shard-0&authSource=admin&retryWrites=true&w=majority")
+mongoose
+  .connect(
+    "mongodb://vedant:BKqA6auni1hM1fRV@ac-b6xpqch-shard-00-00.nnwdei6.mongodb.net:27017,ac-b6xpqch-shard-00-01.nnwdei6.mongodb.net:27017,ac-b6xpqch-shard-00-02.nnwdei6.mongodb.net:27017/?ssl=true&replicaSet=atlas-vq4uy8-shard-0&authSource=admin&retryWrites=true&w=majority"
+  )
   .then(() => console.log("Database connected!"))
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
 
 // app.post("/register", async (req, res) => {
 //   const { username, password } = req.body;
@@ -22,11 +27,28 @@ mongoose.connect( "mongodb://vedant:iaK7qavvID8cbhyl@ac-dgelwon-shard-00-00.owb0
 
 app.post("/register", async (req, res) => {
   const { username, password } = req.body;
+  try {
     const userDoc = await User.create({
       username,
-      password,
+      password: bcrypt.hashSync(password, salt),
     });
     res.json(userDoc);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const userDoc = await User.findOne({ username });
+  const passok = bcrypt.compare(password, userDoc.password);
+  if (passok) {
+    
+    // res.json();
+  } else {
+    res.status(400).json("Invalid password");
+  }
+    
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,14 +1,34 @@
-import React from "react";
+import {useContext, useState} from "react";
+import {Navigate} from "react-router-dom";
+import {UserContext} from "../UserContext";
 
-const LoginPage = () => {
+export default function LoginPage() {
+  const [username,setUsername] = useState('');
+  const [password,setPassword] = useState('');
+  const [redirect,setRedirect] = useState(false);
+  const {setUserInfo} = useContext(UserContext);
+  async function login(ev) {
+    ev.preventDefault();
+    const response = await fetch('http://localhost:4000/login', {
+      method: 'POST',
+      body: JSON.stringify({username, password}),
+      headers: {'Content-Type':'application/json'},
+      credentials: 'include',
+    });
+  }
+
   return (
-    <form action="" className="login">
-        <h1>Login</h1>
-      <input type="text" placeholder="username" />
-      <input type="password" placeholder="password" />
-      <button type="submit">Login</button>
+    <form className="login" onSubmit={login}>
+      <h1>Login</h1>
+      <input type="text"
+             placeholder="username"
+             value={username}
+             onChange={ev => setUsername(ev.target.value)}/>
+      <input type="password"
+             placeholder="password"
+             value={password}
+             onChange={ev => setPassword(ev.target.value)}/>
+      <button>Login</button>
     </form>
   );
-};
-
-export default LoginPage;
+}
