af1460a6b9bf941d42015dcd7feed36d2f382bb2
vedant-3010 <vedantdandgepro@gmail.com>
2023-10-21 17:19:21 +0530

    all blog operations added

--- client/src/Post.js
+++ client/src/Post.js
@@ -1,33 +1,32 @@
-import React from 'react'
+import { formatISO9075 } from "date-fns";
+import { Link } from "react-router-dom";
 
-const Post = () => {
-    return (
-        <div className="post">
-            <div className="image">
-                <img
-                    src="https://res.cloudinary.com/didf67ktw/image/upload/v1676030251/cld-sample.jpg"
-                    alt="logo"
-                />
-            </div>
-
-            <div className="texts">
-                <h2>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Non,
-                    sapiente.
-                </h2>
-                <p className="info">
-                    <a href="" className="author">
-                        vecdant dandge
-                    </a>
-                    <time> 2023-01-06 16:45</time>
-                </p>
-                <p className="summary">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique,
-                    inventore odio! Quo cumque ipsum quos vel eaque dolores hic! Dolor.
-                </p>
-            </div>
-        </div>
-    )
+export default function Post({
+  _id,
+  title,
+  summary,
+  cover,
+  content,
+  createdAt,
+  author,
+}) {
+  return (
+    <div className="post">
+      <div className="image">
+        <Link to={`/post/${_id}`}>
+          <img src={"http://localhost:4000/" + cover} alt="" />
+        </Link>
+      </div>
+      <div className="texts">
+        <Link to={`/post/${_id}`}>
+          <h2>{title}</h2>
+        </Link>
+        <p className="info">
+          <a className="author">{author.username}</a>
+          <time>{formatISO9075(new Date(createdAt))}</time>
+        </p>
+        <p className="summary">{summary}</p>
+      </div>
+    </div>
+  );
 }
-
-export default Post
